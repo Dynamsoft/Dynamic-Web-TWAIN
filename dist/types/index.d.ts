a368d1cab3a7195c0d3bb3a6f46e5face1b765a8
DMGithubPublisher <github@dynamsoft.com>
2026-02-10 22:58:19 -0800

    update to web-twain 19.3.1

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -6,9 +6,9 @@
 * Product: Dynamsoft Web Twain
 * Web Site: http://www.dynamsoft.com
 *
-* Copyright 2025, Dynamsoft Corporation
+* Copyright 2026, Dynamsoft Corporation
 * Author: Dynamsoft Support Team
-* Version: 19.2
+* Version: 19.3.1
 */
 import Dynamsoft from "./Dynamsoft";
 export default Dynamsoft;
